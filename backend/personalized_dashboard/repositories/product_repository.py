23a6292b9ef852eed7337bb036765961094e7bec
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-17 01:32:41 +0530

    commited by n8n

--- backend/personalized_dashboard/repositories/product_repository.py
+++ backend/personalized_dashboard/repositories/product_repository.py
@@ -1,9 +1,8 @@
 # Epic Title: Display Personalized Banking Products
 
 from backend.personalized_dashboard.models.product import Product
-from typing import List
 
 class ProductRepository:
 
-    def get_all_products(self) -> List[Product]:
+    def get_all_products(self) -> list[Product]:
         return Product.query.all()
