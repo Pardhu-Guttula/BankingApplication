b55d6a6ebebbd816a3ac9adbd386d368e14679b4
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-16 23:26:33 +0530

    arch-gen

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,84 +1,87 @@
-provider "google" {
-  project = var.project_id
-  region  = var.region
-}
-
-resource "google_project_service" "gcs" {
-  project = var.project_id
-  service = "storage.googleapis.com"
-}
-
-resource "google_project_service" "cloudfunctions" {
-  project = var.project_id
-  service = "cloudfunctions.googleapis.com"
-}
-
-resource "google_project_service" "sql" {
-  project = var.project_id
-  service = "sqladmin.googleapis.com"
-}
-
-resource "google_storage_bucket" "banking_portal_bucket" {
-  name     = "${var.project_id}-bucket"
-  location = var.region
-}
-
-resource "google_cloudfunctions_function" "banking_portal_function" {
-  name        = "bankingPortalFunction"
-  description = "Backend Cloud Function for the banking portal"
-  runtime     = "nodejs16"
-  entry_point = "function"
-
-  event_trigger {
-    event_type = "google.storage.object.finalize"
-    resource   = google_storage_bucket.banking_portal_bucket.name
+provider {
+  aws {
+    region = var.aws_region
   }
 }
 
-resource "google_sql_database_instance" "banking_portal_instance" {
-  name             = "banking-portal-db"
-  database_version = "MYSQL_5_7"
+resource "aws_elastic_beanstalk_application" "app" {
+  name        = "banking-portal"
+  description = "Banking portal application"
+}
 
-  settings {
-    tier = "db-f1-micro"
+resource "aws_elastic_beanstalk_environment" "frontend" {
+  name                = "banking-frontend"
+  application         = aws_elastic_beanstalk_application.app.name
+  solution_stack_name = "64bit Amazon Linux 2018.03 v2.9.9 running Python 3.7"
+}
+
+resource "aws_cloudfront_distribution" "frontend_dist" {
+  origin {
+    domain_name = aws_elastic_beanstalk_environment.frontend.endpoint_url
+    origin_id   = "eb-origin"
   }
+  enabled = true
+  default_root_object = "index.html"
 }
 
-resource "google_sql_database" "banking_portal_database" {
-  name     = "banking_db"
-  instance = google_sql_database_instance.banking_portal_instance.name
+resource "aws_lambda_function" "backend_lambda" {
+  filename         = var.lambda_zip
+  function_name    = "backend-function"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "lambda_function.lambda_handler"
+  source_code_hash = filebase64sha256(var.lambda_zip)
+  runtime          = "python3.8"
 }
 
-resource "google_compute_backend_service" "default" {
-  name                  = "default-backend-service"
-  enable_cdn            = true
-  health_checks         = [google_compute_http_health_check.default.name]
-  session_affinity      = "CLIENT_IP"
-  connection_draining {
-    draining_timeout_sec = 300
-  }
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
 }
 
-resource "google_compute_http_health_check" "default" {
-  name                = "default-health-check"
-  request_path        = "/"
-  check_interval_sec  = 5
-  timeout_sec         = 5
-  healthy_threshold   = 2
-  unhealthy_threshold = 2
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-resource "google_compute_url_map" "banking_portal_url_map" {
-  default_service  = google_compute_backend_service.default.self_link
+resource "aws_rds_instance" "db_instance" {
+  allocated_storage    = 20
+  db_name              = var.db_name
+  engine               = "mysql"
+  instance_class       = "db.t2.micro"
+  username             = var.db_username
+  password             = var.db_password
+  parameter_group_name = "default.mysql5.7"
 }
 
-resource "google_compute_target_http_proxy" "banking_portal_http_proxy" {
-  url_map = google_compute_url_map.banking_portal_url_map.self_link
+resource "aws_s3_bucket" "data_bucket" {
+  bucket = "banking-portal-data"
+  acl    = "private"
 }
 
-resource "google_compute_forwarding_rule" "banking_portal_forwarding_rule" {
-  name        = "http-content-rule"
-  target      = google_compute_target_http_proxy.banking_portal_http_proxy.self_link
-  port_range  = "80"
-  load_balancing_scheme = "EXTERNAL"
+resource "aws_sqs_queue" "integration_queue" {
+  name = "integration-queue"
+}
+
+resource "aws_lambda_event_source_mapping" "sqs_event" {
+  event_source_arn = aws_sqs_queue.integration_queue.arn
+  function_name    = aws_lambda_function.backend_lambda.arn
+  batch_size       = 10
+}
+
+resource "aws_s3_bucket" "data_bucket" {
+  bucket = "banking-portal-data"
+  acl    = "private"
+}
+
+resource "aws_sqs_queue" "integration_queue" {
+  name = "integration-queue"
 }
