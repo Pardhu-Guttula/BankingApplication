7359d0c546c03f6799f8e1d40ef96f44ca9229e9
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-16 15:15:34 +0530

    arch-gen

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,16 @@
+variable "location" {
+  description = "The location where resources will be created."
+  type        = string
+  default     = "West Europe"
+}
+
+variable "sql_admin_username" {
+  description = "The username for the SQL Server admin."
+  type        = string
+}
+
 variable "sql_admin_password" {
-  description = "The password for the SQL admin user"
+  description = "The password for the SQL Server admin."
   type        = string
   sensitive   = true
 }
