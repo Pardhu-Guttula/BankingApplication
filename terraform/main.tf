1321231c6dc7a91bfdc3e5cace6cf069142dc6a1
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-16 22:23:54 +0530

    arch-gen

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,64 +1,69 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.56.0"
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_project_service" "identity_platform" {
+  service = "identityplatform.googleapis.com"
+}
+
+resource "google_project_service" "cloud_functions" {
+  service = "cloudfunctions.googleapis.com"
+}
+
+resource "google_project_service" "sqladmin" {
+  service = "sqladmin.googleapis.com"
+}
+
+resource "google_project_iam_custom_role" "frontend_access" {
+  role_id     = "frontendAccess"
+  title       = "Frontend Access Role"
+  permissions = [
+    "iam.serviceAccounts.actAs",
+  ]
+}
+
+resource "google_identity_platform_default_config" "default" {}
+
+resource "google_cloudfunctions_function" "backend" {
+  name        = "backend"
+  runtime     = "nodejs16"
+  entry_point = "functionName"
+  source_archive_bucket = var.source_archive_bucket
+  source_archive_object = var.source_archive_object
+  trigger_http            = true
+  available_memory_mb   = 256
+  timeout               = 60
+}
+
+resource "google_sql_database_instance" "db_instance" {
+  name = "instance"
+  database_version = "MYSQL_8_0"
+  settings {
+    tier = "db-f1-micro"
+  }
+}
+
+resource "google_sql_database" "database" {
+  name = "banking"
+  instance = google_sql_database_instance.db_instance.name
+}
+
+resource "google_api_gateway_api" "api" {
+  api_id = "gateway"
+}
+
+resource "google_api_gateway_api_config" "api_config" {
+  api      = google_api_gateway_api.api.api_id
+  openapi_documents {
+    document {
+      path     = "openapi-spec.json"
+      contents = file("openapi-spec.json")
     }
   }
-  backend "local" {}
+  project = var.project_id
 }
 
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = var.location
-}
-
-resource "azurerm_app_service_plan" "example" {
-  name                = "example-appserviceplan"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  sku_name            = "B1"
-}
-
-resource "azurerm_app_service" "example" {
-  name                = "example-appservice"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
-}
-
-resource "azurerm_api_management" "example" {
-  name                = "example-api"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  publisher_name      = "My Company"
-  publisher_email     = "company@example.com"
-  sku_name            = "Developer_1"
-}
-
-resource "azurerm_sql_server" "example" {
-  name                = "example-sqlserver"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  version             = "12.0"
-  administrator_login = var.sql_admin_username
-  administrator_login_password = var.sql_admin_password
-}
-
-resource "azurerm_sql_database" "example" {
-  name                = "example-sqldatabase"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  server_name         = azurerm_sql_server.example.name
-  requested_service_objective_name = "S0"
-}
-
-resource "azurerm_active_directory_b2c_directory" "example" {
-  name                = "example-b2c-directory"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+resource "google_api_gateway_gateway" "gateway" {
+  api_config = google_api_gateway_api_config.api_config.id
 }
