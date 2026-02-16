7e03137582d16612ab7eaab49ffe30a373254715
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-17 00:25:30 +0530

    commited by n8n

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-# Epic Title: Intuitive Dashboard Interface
+# Epic Title: Display Personalized Banking Products
 
 import logging
 from flask import Flask
 from backend.database import db
-from backend.personalized_dashboard.controllers.dashboard_ui_controller import dashboard_ui_controller
+from backend.personalized_dashboard.controllers.dashboard_controller import dashboard_controller
 
 def create_app() -> Flask:
     app = Flask(__name__)
@@ -11,7 +11,7 @@ def create_app() -> Flask:
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
-    app.register_blueprint(dashboard_ui_controller, url_prefix='/ui')
+    app.register_blueprint(dashboard_controller, url_prefix='/dashboard')
 
     return app
 
