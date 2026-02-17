354edd2fb6ebec80e796f8721e126084ab79c7c8
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-18 01:44:06 +0530

    Enable server configuration in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,12 +5,12 @@ export default defineConfig({
   plugins: [react()],
 
   // base: './',
-  // server: {
-  //   host: '0.0.0.0',
-  //   port: 5173,
-  //   allowedHosts: true,
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: true,
   //   hmr: {
   //     path: '/c0b9164a-76ec-4978-adcc-71df6e601857/preview',
     // }
-  // }
+  }
 })
