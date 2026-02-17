3c3bd5777027c065c94b47f9a01cd435c273eda4
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-18 01:29:09 +0530

    Update Vite base path and HMR path configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
-  base: process.env.VITE_BASE_PATH||'/',
+  base: '/c0b9164a-76ec-4978-adcc-71df6e601857/preview',
   server: {
     host: '0.0.0.0',
     port: 5173,
     allowedHosts: true,
     hmr: {
-      path: process.env.VITE_BASE_PATH||'/',
+      path: '/c0b9164a-76ec-4978-adcc-71df6e601857/preview',
     }
   }
 })
