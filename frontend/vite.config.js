40b1bef657f2e8338024d0008abdd89382040a5d
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-18 01:23:39 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
-  base: '${VITE_BASE_PATH}',
+  base: process.env.VITE_BASE_PATH||'/',
   server: {
     host: '0.0.0.0',
     port: 5173,
