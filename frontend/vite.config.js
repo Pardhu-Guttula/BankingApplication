0392674d1e5c60d1703549059256f40f47f8fdde
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-18 01:07:00 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  host: true,
-  allowedHosts: 'all',
+  
   plugins: [react()],
   base:'./'
+
+   server: {
+    host: true,
+    allowedHosts: 'all'
+  }
 })
