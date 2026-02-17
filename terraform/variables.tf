bf74f6adc3d46aded5f0fc80e5eda2bd92744a84
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-17 17:43:29 +0530

    arch-gen

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,5 @@
-variable "sql_admin_username" {
-  description = "The administrator username for the SQL server."
-  type        = string
-}
-
-variable "sql_admin_password" {
-  description = "The administrator password for the SQL server."
+variable "sql_password" {
+  description = "SQL Server password"
   type        = string
   sensitive   = true
 }
