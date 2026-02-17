98907137bdd2197615aaf1908a9b4e8bdb502eb2
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-18 01:40:29 +0530

    Disable base and server settings in vite config
    Comment out base URL and server configuration.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
-  base: '/c0b9164a-76ec-4978-adcc-71df6e601857/preview',
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    allowedHosts: true,
-    hmr: {
-      path: '/c0b9164a-76ec-4978-adcc-71df6e601857/preview',
-    }
-  }
+  // base: './',
+  // server: {
+  //   host: '0.0.0.0',
+  //   port: 5173,
+  //   allowedHosts: true,
+  //   hmr: {
+  //     path: '/c0b9164a-76ec-4978-adcc-71df6e601857/preview',
+    // }
+  // }
 })
