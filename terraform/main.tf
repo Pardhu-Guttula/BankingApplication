007fbe17ab57e96adaff3859f07990c1a45f177b
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-18 13:52:34 +0530

    arch-gen

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.56.0"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
 provider "azurerm" {
   features {}
 }
 
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
-  location = "West Europe"
-}
-
-resource "azurerm_app_service" "app" {
-  name                = "example-appservice"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
+  location = var.location
 }
 
 resource "azurerm_app_service_plan" "example" {
-  name                = "example-appservice-plan"
+  name                = "example-app-service-plan"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   sku {
@@ -24,72 +27,119 @@ resource "azurerm_app_service_plan" "example" {
   }
 }
 
-resource "azurerm_active_directory_domain_service" "example" {
-  name                = "example-aad"
+resource "azurerm_app_service" "example" {
+  name                = "example-app-service"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  domain_name         = "example.com"
-}
-
-resource "azurerm_storage_account" "example" {
-  name                     = "examplestoracc"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  app_service_plan_id = azurerm_app_service_plan.example.id
 }
 
 resource "azurerm_sql_server" "example" {
-  name                         = "examplesqlserver"
-  resource_group_name          = azurerm_resource_group.example.name
-  location                     = azurerm_resource_group.example.location
-  version                      = "12.0"
-  administrator_login          = "admin"
-  administrator_login_password = var.sql_password
-}
-
-resource "azurerm_key_vault" "example" {
-  name                        = "examplekeyvault"
-  location                    = azurerm_resource_group.example.location
-  resource_group_name         = azurerm_resource_group.example.name
-}
-
-resource "azurerm_api_management" "example" {
-  name                = "example-apim"
-  location            = azurerm_resource_group.example.location
+  name                = "example-sql-server"
   resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  version             = "12.0"
+  administrator_login = var.sql_admin_username
+  administrator_login_password = var.sql_admin_password
 }
 
-resource "azurerm_signalr_service" "example" {
-  name                = "example-signalr"
-  location            = azurerm_resource_group.example.location
+resource "azurerm_sql_database" "example" {
+  name                = "example-sql-database"
   resource_group_name = azurerm_resource_group.example.name
-  sku {
-    name     = "Free_F1"
-    capacity = 1
+  location            = azurerm_resource_group.example.location
+  server_name         = azurerm_sql_server.example.name
+  sku_name            = "S0"
+}
+
+resource "azurerm_cosmosdb_account" "example" {
+  name                = "example-cosmosdb-account"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+  consistency_policy {
+    consistency_level = "Session"
+  }
+  geo_location {
+    location          = azurerm_resource_group.example.location
+    failover_priority = 0
   }
 }
 
-resource "azurerm_communication_service" "example" {
-  name                = "example-comms"
+resource "azurerm_api_management" "example" {
+  name                = "example-api-management"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
+  publisher_name      = "example"
+  publisher_email     = "admin@example.com"
+  sku_name            = "Developer_1"
 }
 
-resource "azurerm_content_moderator" "example" {
-  name                = "example-contentmod"
+resource "azurerm_service_bus_namespace" "example" {
+  name                = "example-service-bus-ns"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
+  sku                 = "Standard"
 }
 
-resource "azurerm_logic_app" "example" {
-  name                = "example-logicapp"
-  location            = azurerm_resource_group.example.location
+resource "azurerm_service_bus_queue" "example" {
+  name                = "example-queue"
   resource_group_name = azurerm_resource_group.example.name
+  namespace_name      = azurerm_service_bus_namespace.example.name
 }
 
-resource "azurerm_devops_project" "example" {
-  name                = "example-devops"
+resource "azurerm_application_gateway" "example" {
+  name                = "example-app-gateway"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku {
+    name     = "WAF_Medium"
+    tier     = "WAF_v2"
+    capacity = 2
+  }
+  gateway_ip_configuration {
+    name      = "example-gateway-ip-configuration"
+    subnet_id = azurerm_subnet.example.id
+  }
+  frontend_port {
+    name = "frontendPort"
+    port = 443
+  }
+  frontend_ip_configuration {
+    name                 = "frontendIpConfiguration"
+    public_ip_address_id = azurerm_public_ip.example.id
+  }
+  backend_address_pool {
+    name = "example-backend-address-pool"
+  }
+  backend_http_settings {
+    name                  = "example-backend-http-settings"
+    cookie_based_affinity = "Disabled"
+    port                  = 80
+    protocol              = "Http"
+  }
+  http_listener {
+    name                           = "example-http-listener"
+    frontend_ip_configuration_name = "frontendIpConfiguration"
+    frontend_port_name             = "frontendPort"
+    protocol                       = "Https"
+    ssl_certificate_name           = "example-ssl-cert"
+  }
+  request_routing_rule {
+    name                       = "example-routing-rule"
+    rule_type                  = "Basic"
+    http_listener_name         = "example-http-listener"
+    backend_address_pool_name  = "example-backend-address-pool"
+    backend_http_settings_name = "example-backend-http-settings"
+  }
+  key_vault_secret_id = var.key_vault_secret_id
+}
+
+resource "azurerm_function_app" "example" {
+  name                = "example-function-app"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+  storage_account_name = azurerm_storage_account.example.name
+  storage_account_access_key  = azurerm_storage_account.example.primary_access_key
 }
