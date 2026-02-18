7d2a76b097e106c06160ca48f56592e634f42baa
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-18 13:52:35 +0530

    arch-gen

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,21 @@
-variable "sql_password" {
-  description = "SQL Server password"
+variable "location" {
+  description = "The location where resources will be created"
   type        = string
-  sensitive   = true
+  default     = "East US"
+}
+
+variable "sql_admin_username" {
+  description = "The administrator username for SQL Server"
+  type        = string
+  default     = "adminUser"
+}
+
+variable "sql_admin_password" {
+  description = "The administrator password for SQL Server"
+  type        = string
+}
+
+variable "key_vault_secret_id" {
+  description = "The ID of the Key Vault secret for SSL certificate"
+  type        = string
 }
