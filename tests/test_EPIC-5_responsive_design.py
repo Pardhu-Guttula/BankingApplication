2624ae96121cd9dcd9f86a39e61050454ce4a891
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-19 17:33:19 +0530

    test: add test cases for Responsive Design

--- tests/test_EPIC-5_responsive_design.py
+++ tests/test_EPIC-5_responsive_design.py
@@ -1,6 +1,20 @@
 import pytest
 
 
-def test_responsive_design():
-    # Add your test code here
-    assert True
+class TestResponsiveDesign:
+
+    def test_page_load(self):
+        response = self.client.get('/')
+        assert response.status_code == 200
+
+    def test_responsive_layout(self):
+        response = self.client.get('/')
+        assert 'meta name="viewport"' in response.data.decode('utf-8')
+
+    def test_element_visibility(self):
+        response = self.client.get('/')
+        assert '<div class="container">' in response.data.decode('utf-8')
+
+    def test_style_inclusion(self):
+        response = self.client.get('/')
+        assert '<link rel="stylesheet" href="/static/css/styles.css">' in response.data.decode('utf-8')
