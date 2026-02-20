fe68aa6464f5a51a84bed7bc4e2aec9721f704af
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-20 15:03:18 +0530

    arch-gen

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,35 @@
+variable "location" {
+  description = "Azure Region"
+  type        = string
+  default     = "West Europe"
+}
+
 variable "resource_group_name" {
-  description = "The name of the resource group"
+  description = "Resource group name"
   type        = string
   default     = "example-resources"
 }
 
-variable "location" {
-  description = "The location of the resources"
+variable "app_service_plan_name" {
+  description = "App service plan name"
   type        = string
-  default     = "East US"
+  default     = "example-appservice-plan"
+}
+
+variable "app_service_name" {
+  description = "App service name"
+  type        = string
+  default     = "example-appservice"
+}
+
+variable "api_management_name" {
+  description = "API management name"
+  type        = string
+  default     = "example-api"
+}
+
+variable "key_vault_name" {
+  description = "Key vault name"
+  type        = string
+  default     = "examplevault"
 }
