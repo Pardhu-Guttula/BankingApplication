f214dc9a4d01c79a0164873c61c5a1b3c9e6dab3
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-20 13:30:33 +0530

    arch-gen

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.56.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
 }
@@ -16,81 +7,85 @@ resource "azurerm_resource_group" "rg" {
   location = "East US"
 }
 
-resource "azurerm_app_service_plan" "frontend_plan" {
-  name                = "frontend-app-service-plan"
+resource "azurerm_app_service_plan" "asp" {
+  name                = "example-app-service-plan"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  kind                = "App"
   sku {
     tier = "Standard"
     size = "S1"
   }
 }
 
-resource "azurerm_app_service" "frontend" {
-  name                = "frontend-app-service"
+resource "azurerm_app_service" "app" {
+  name                = "example-app-service"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.frontend_plan.id
+  app_service_plan_id = azurerm_app_service_plan.asp.id
+  site_config {
+    always_on = true
+    linux_fx_version = "PYTHON|3.9"
+  }
+  identity {
+    type = "SystemAssigned"
+  }
 }
 
-resource "azurerm_static_site" "frontend_static" {
-  name                = "frontend-static-web"
+resource "azurerm_sql_server" "sqlserver" {
+  name                = "mysqldbserver"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  administrator_login          = "sqladmin"
+  administrator_login_password = "Password123"
+}
+
+resource "azurerm_sql_database" "sqldb" {
+  name                = "mysqldb"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  server_name         = azurerm_sql_server.sqlserver.name
+  sku_name            = "Standard"
+  size                = "S1"
+  max_size_gb         = 10
+}
+
+resource "azurerm_active_directory_domain_service" "addomain" {
+  name                = "example-aadds"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-}
-
-resource "azurerm_api_management" "api_management" {
-  name                = "api-management"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  publisher_name      = "Example Publisher"
-  publisher_email     = "publisher@example.com"
-  sku_name            = "Developer"
-}
-
-resource "azurerm_app_service" "api_app" {
-  name                = "api-app-service"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.frontend_plan.id
-}
-
-resource "azurerm_key_vault" "keyvault" {
-  name                = "example-keyvault"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  tenant_id           = var.tenant_id
-  sku_name            = "standard"
-}
-
-resource "azurerm_key_vault_access_policy" "keyvault_access" {
-  key_vault_id = azurerm_key_vault.keyvault.id
-  tenant_id    = var.tenant_id
-  object_id    = var.object_id
-  secret_permissions = [
-    "get",
-    "list",
-  ]
-}
-
-resource "azurerm_b2c_directory" "b2c" {
-  name     = "exampleb2c"
-  location = "Global"
   sku {
     tier = "Standard"
   }
+  domain_name         = "example.com"
 }
 
-resource "azurerm_monitor_diagnostic_setting" "monitor" {
-  name               = "example-diagnostic"
-  target_resource_id = azurerm_api_management.api_management.id
-  log_analytics_workspace_id = var.log_analytics_workspace_id
+resource "azurerm_monitor_diagnostic_setting" "diag" {
+  name               = "example-diagnostics"
+  target_resource_id = azurerm_app_service.app.id
   enabled_log {
-    category = "GatewayLogs"
+    category = "AppServiceHTTPLogs"
     enabled  = true
+    retention_policy {
+      enabled = false
+    }
   }
-  metric {
-    category = "AllMetrics"
-    enabled  = true
+}
+
+resource "azurerm_key_vault" "kv" {
+  name                = "examplekv"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku_name            = "standard"
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+    secret_permissions = [
+      "get",
+      "list",
+      "set",
+      "delete"
+    ]
   }
 }
