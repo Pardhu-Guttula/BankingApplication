655ff5faa7d7420dfd22a970083ce7f70fe7fa5d
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-20 12:57:40 +0530

    arch-gen

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,39 @@
-variable "sql_admin_username" {
-  type = string
-}
-
-variable "sql_admin_password" {
-  type = string
-}
-
-variable "user_object_id" {
-  type = string
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group"
 }
 
 variable "location" {
-  type    = string
-  default = "East US"
+  type        = string
+  description = "Location for all resources"
+}
+
+variable "sql_server_name" {
+  type        = string
+  description = "SQL Server Name"
+}
+
+variable "sql_admin_login" {
+  type        = string
+  description = "SQL Admin Login"
+}
+
+variable "sql_admin_password" {
+  type        = string
+  description = "SQL Admin Password"
+}
+
+variable "sql_db_name" {
+  type        = string
+  description = "SQL Database Name"
+}
+
+variable "storage_account_name" {
+  type        = string
+  description = "Storage Account Name"
+}
+
+variable "b2c_name" {
+  type        = string
+  description = "B2C Tenant Name"
 }
