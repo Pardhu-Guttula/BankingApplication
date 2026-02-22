21a7964708bbcf656e2509e78cd67dfe55a313c4
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-23 02:13:44 +0530

    commited by n8n

---
+++ backend/shopping_cart/models/shopping_cart.py
@@ -0,0 +1,11 @@
+# Epic Title: Add products to the shopping cart
+
+from dataclasses import dataclass
+from typing import List
+from backend.shopping_cart.models.cart_item import CartItem
+from backend.user_profile.models.user import User
+
+@dataclass
+class ShoppingCart:
+    user: User
+    items: List[CartItem]
