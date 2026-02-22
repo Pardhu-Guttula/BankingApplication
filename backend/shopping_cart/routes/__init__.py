7033714f82360e433634f8592ea217eed64ca99b
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-23 02:14:39 +0530

    commited by n8n

--- backend/shopping_cart/routes/__init__.py
+++ backend/shopping_cart/routes/__init__.py
@@ -1,4 +1,4 @@
-# Epic Title: Update product quantities in the shopping cart
+# Epic Title: Remove products from the shopping cart
 
 from flask import Flask
 from backend.shopping_cart.controllers.shopping_cart_controller import shopping_cart_bp
