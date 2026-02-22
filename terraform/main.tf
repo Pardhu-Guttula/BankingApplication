5cd459cb1101306a6c26b0a43a61f9e0233d4ef3
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-23 00:12:28 +0530

    arch-gen

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,69 +1,101 @@
-provider "azurerm" {
-  features {}
+terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 4.56.0"
     }
   }
+  required_version = ">= 1.2.0"
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+provider "azurerm" {
+  features {}
 }
 
-resource "azurerm_app_service_plan" "example" {
-  name                = "example-appserviceplan"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Azure Active Directory B2C
+resource "azurerm_b2c_directory" "b2c" {
+  name                = var.b2c_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
+
+# Azure Key Vault
+resource "azurerm_key_vault" "kv" {
+  name                = var.key_vault_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku_name            = "standard"
+
+  tenant_id = var.tenant_id
+}
+
+# Azure App Service for Web and Mobile App
+resource "azurerm_app_service_plan" "asp" {
+  name                = "${var.project_name}-asp"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   sku {
-    tier = "Standard"
-    size = "S1"
+    tier = "Free"
+    size = "F1"
   }
 }
 
-resource "azurerm_app_service" "example" {
-  name                = "example-appservice"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
-  site_config {
-    dotnet_framework_version = "v4.0"
-    scm_type                 = "LocalGit"
-  }
-  app_settings = {
-    "WEBSITE_RUN_FROM_PACKAGE" = "1"
-  }
+resource "azurerm_app_service" "web_app" {
+  name                = "${var.project_name}-web"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.asp.id
 }
 
-resource "azurerm_sql_server" "example" {
-  name                         = "mysqlserver"
-  resource_group_name          = azurerm_resource_group.example.name
-  location                     = azurerm_resource_group.example.location
+resource "azurerm_app_service" "mobile_app" {
+  name                = "${var.project_name}-mobile"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.asp.id
+}
+
+# Azure SQL Database
+resource "azurerm_sql_server" "sql" {
+  name                         = "${var.project_name}-sql-server"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
   version                      = "12.0"
-  administrator_login          = var.sql_administrator_login
-  administrator_login_password = var.sql_administrator_password
+  administrator_login          = var.sql_admin_username
+  administrator_login_password = var.sql_admin_password
 }
 
-resource "azurerm_sql_database" "example" {
-  name                = "mysqldatabase"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  server_name         = azurerm_sql_server.example.name
-  sku_name            = "S0"
+resource "azurerm_sql_database" "sqldb" {
+  name                = "${var.project_name}-db"
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_sql_server.sql.name
+  edition             = "Basic"
+  requested_service_objective_name = "Basic"
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "examplestoracc"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+# Azure Blob Storage
+resource "azurerm_storage_account" "storage" {
+  name                     = "${var.project_name}storage"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_storage_container" "example" {
-  name                  = "vhds"
-  storage_account_name  = azurerm_storage_account.example.name
+resource "azurerm_storage_container" "container" {
+  name                  = "content"
+  storage_account_name  = azurerm_storage_account.storage.name
   container_access_type = "private"
+}
+
+# Azure Logic Apps
+resource "azurerm_logic_app" "logic_app" {
+  name                = "${var.project_name}-logicapp"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
