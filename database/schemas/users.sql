9b455d0cec52160a2c051579f1d17780a737366c
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-23 02:18:21 +0530

    commited by n8n

--- database/schemas/users.sql
+++ database/schemas/users.sql
@@ -1,9 +1,7 @@
-# Epic Title: Update Personal Information
+# Epic Title: Change Password
 
 CREATE TABLE IF NOT EXISTS users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    address VARCHAR(255) NOT NULL,
-    updated_at TIMESTAMP NOT NULL
+    hashed_password VARCHAR(255) NOT NULL
 );
