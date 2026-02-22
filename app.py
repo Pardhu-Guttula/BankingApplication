4bd6ea18da80112f269538aee726a7789723f138
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-23 02:18:52 +0530

    commited by n8n

--- app.py
+++ app.py
@@ -2,9 +2,7 @@
 
 from backend.authentication.controllers.login_controller import app as login_app
 from backend.authentication.controllers.logout_controller import app as logout_app
-from backend.access_control.controllers.role_controller import app as role_app
 
 if __name__ == "__main__":
     login_app.run(debug=True)
     logout_app.run(debug=True)
-    role_app.run(debug=True)
