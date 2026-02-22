5e1541ce8e2299c9a38d30c55a50d1d4a23e74b1
KGSSV AKHIL KUMAR <53571917+KGSSV@users.noreply.github.com>
2026-02-22 23:19:02 +0530

    Update account_lockout_controller.py

--- backend/authentication/controllers/account_lockout_controller.py
+++ backend/authentication/controllers/account_lockout_controller.py
@@ -1,4 +1,4 @@
-# Epic Title: User Authentication and Security
+# Epic Title: User Authentication and Authorization
 
 from flask import Blueprint, request, jsonify
 from backend.authentication.services.account_lockout_service import AccountLockoutService
