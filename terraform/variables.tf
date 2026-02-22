2b46c9e9109d63149416c5f4a68119becc8cc5c0
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-23 04:08:26 +0530

    arch-gen

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,27 @@
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+  default     = "self-service-rg"
+}
+
+variable "location" {
+  description = "Location for all resources"
+  type        = string
+  default     = "West Europe"
+}
+
+variable "tenant_id" {
+  description = "Tenant ID for Azure"
+  type        = string
+}
+
 variable "sql_admin_username" {
-  description = "The administrator username for the SQL Server"
+  description = "SQL Server admin username"
   type        = string
 }
 
 variable "sql_admin_password" {
-  description = "The administrator password for the SQL Server"
+  description = "SQL Server admin password"
   type        = string
   sensitive   = true
 }
