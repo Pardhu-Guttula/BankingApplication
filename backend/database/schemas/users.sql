442a9a1dd814dd9a6eb720a33a5c25271d72f3ea
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-23 02:18:49 +0530

    commited by n8n

--- backend/database/schemas/users.sql
+++ backend/database/schemas/users.sql
@@ -4,5 +4,6 @@ CREATE TABLE users (
     user_id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    hashed_password VARCHAR(255) NOT NULL
+    role_id INT NOT NULL,
+    FOREIGN KEY(role_id) REFERENCES roles(role_id)
 );
