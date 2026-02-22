ee0e07636513c45cabfca06b4a57cadc54e4ac30
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-23 01:58:25 +0530

    arch-gen

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,41 +1,10 @@
-variable "project_name" {
-  description = "The name of the project."
-  type        = string
-}
-
-variable "resource_group_name" {
-  description = "The name of the Resource Group."
-  type        = string
-}
-
-variable "location" {
-  description = "The Azure Region where resources will be located."
-  type        = string
-  default     = "East US"
-}
-
-variable "b2c_name" {
-  description = "The name of the Azure B2C directory."
-  type        = string
-}
-
-variable "key_vault_name" {
-  description = "The name of the Azure Key Vault."
-  type        = string
-}
-
-variable "tenant_id" {
-  description = "The Tenant ID for the Key Vault."
-  type        = string
-}
-
 variable "sql_admin_username" {
-  description = "The SQL Server administrator username."
+  description = "The administrator username for the SQL Server"
   type        = string
 }
 
 variable "sql_admin_password" {
-  description = "The SQL Server administrator password."
+  description = "The administrator password for the SQL Server"
   type        = string
   sensitive   = true
 }
