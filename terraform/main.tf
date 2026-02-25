8c55d59cbd9f3e03c24b39969c0639ddeb4f2548
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-25 17:21:34 +0530

    arch-gen

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,61 +4,85 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 4.56.0"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
   }
-  required_version = ">= 0.14.9"
 }
 
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+provider "aws" {
+  region = var.aws_region
 }
 
-resource "azurerm_storage_account" "storage" {
-  name                     = "examplestoracc"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = var.azure_location
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-appserviceplan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "example-appservice"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+}
+
+resource "azurerm_sql_server" "example" {
+  name                         = "example-sqlserver"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = var.sql_admin_username
+  administrator_login_password = var.sql_admin_password
+}
+
+resource "azurerm_sql_database" "example" {
+  name                = "example-sqldatabase"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  server_name         = azurerm_sql_server.example.name
+  sku_name            = "S0"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestorageacc"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_postgresql_server" "postgresql" {
-  name                = "examplepgserver"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  administrator_login = var.db_admin_login
-  administrator_login_password = var.db_admin_password
-  version             = "11"
-  sku_name            = "GP_Gen5_2"
-  storage_profile {
-    storage_mb            = 5120
-    backup_retention_days = 7
-    geo_redundant_backup  = "Disabled"
-  }
-  public_network_access_enabled = true
-  ssl_enforcement_enabled = true
+resource "aws_instance" "example" {
+  ami           = var.aws_ami_id
+  instance_type = "t2.micro"
 }
 
-resource "azurerm_postgresql_database" "exampledb" {
-  name                = "exampledb"
-  resource_group_name = azurerm_resource_group.rg.name
-  server_name         = azurerm_postgresql_server.postgresql.name
-  charset             = "UTF8"
-  collation           = "English_United States.1252"
+resource "aws_s3_bucket" "example" {
+  bucket = "example-s3-bucket"
+  acl    = "private"
 }
 
-resource "azurerm_role_assignment" "example" {
-  scope                = azurerm_postgresql_server.postgresql.id
-  role_definition_name = "Contributor"
-  principal_id         = var.client_id
-}
-
-resource "azurerm_application_insights" "ai" {
-  name                = "exampleai"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  application_type    = "web"
+resource "aws_db_instance" "example" {
+  identifier              = "example-db-instance"
+  allocated_storage       = 10
+  engine                  = "mysql"
+  instance_class          = "db.t2.micro"
+  name                    = "exampledb"
+  master_username         = var.db_username
+  master_password         = var.db_password
+  skip_final_snapshot     = true
 }
