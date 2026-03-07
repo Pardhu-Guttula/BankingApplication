5e28d24eb18368623d0ebb2ec0eb5fdd522c6e22
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-03-07 22:25:10 +0530

    commited by n8n

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 # Epic Title: Banking Platform — Core API
 
 from flask import Flask
-from backend.controllers.layout.layout_controller import layout_bp
+from backend.controllers.interaction_history.interaction_controller import interaction_bp
 
 app = Flask(__name__)
-app.register_blueprint(layout_bp, url_prefix='/api')
+app.register_blueprint(interaction_bp, url_prefix='/api')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
