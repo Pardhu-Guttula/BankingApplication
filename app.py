5e70caf640487699b74dbaf2500747c7d62a12e5
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-03-07 22:32:13 +0530

    commited by n8n

--- app.py
+++ app.py
@@ -2,11 +2,9 @@
 
 from flask import Flask
 from backend.controllers.interaction_history.interaction_controller import interaction_bp
-from backend.controllers.notifications.notification_controller import notification_bp
 
 app = Flask(__name__)
-app.register_blueprint(interaction_bp, url_prefix='/api/interactions')
-app.register_blueprint(notification_bp, url_prefix='/api/notifications')
+app.register_blueprint(interaction_bp, url_prefix='/api')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
