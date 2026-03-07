70f2c4752accbf4abaf81ce846b0ffa4279c6767
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-03-07 22:34:40 +0530

    commited by n8n

--- backend/models/layout.py
+++ backend/models/layout.py
@@ -2,13 +2,11 @@
 
 from typing import List
 from backend.models.component import Component
-from backend.models.breakpoint import Breakpoint
 
 class Layout:
-    def __init__(self, name: str, components: List[Component], breakpoints: List[Breakpoint]):
+    def __init__(self, name: str, components: List[Component]):
         self.name = name
         self.components = components
-        self.breakpoints = breakpoints
 
     def add_component(self, component: Component):
         self.components.append(component)
@@ -19,26 +17,5 @@ class Layout:
     def get_components(self) -> List[Component]:
         return self.components
 
-    def add_breakpoint(self, breakpoint: Breakpoint):
-        self.breakpoints.append(breakpoint)
-
-    def remove_breakpoint(self, breakpoint: Breakpoint):
-        self.breakpoints.remove(breakpoint)
-
-    def get_breakpoints(self) -> List[Breakpoint]:
-        return self.breakpoints
-
     def align_components(self):
         self.components.sort(key=lambda component: component.get_position())
-
-    def check_consistency(self) -> bool:
-        if not self.components:
-            return True
-        
-        first_component = self.components[0]
-        for component in self.components[1:]:
-            if (component.margin != first_component.margin or
-                component.padding != first_component.padding or
-                component.font_style != first_component.font_style):
-                return False
-        return True
