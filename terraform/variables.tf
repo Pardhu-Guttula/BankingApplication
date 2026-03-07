03d55c3278e42fadb2403e872d21298d8569a515
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-03-07 11:48:27 +0530

    arch-gen

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,38 +1,60 @@
-variable "azure_location" {
-  description = "The location of the Azure resources"
+variable "resource_group_name" {
+  description = "The name of the resource group"
   type        = string
-  default     = "East US"
 }
 
-variable "aws_region" {
-  description = "The region of the AWS resources"
+variable "location" {
+  description = "The Azure region"
   type        = string
-  default     = "us-east-1"
+  default     = "West Europe"
+}
+
+variable "storage_account_name" {
+  description = "The name of the storage account"
+  type        = string
+}
+
+variable "key_vault_name" {
+  description = "The name of the Key Vault"
+  type        = string
+}
+
+variable "api_management_name" {
+  description = "The name of the API Management service"
+  type        = string
+}
+
+variable "function_app_name" {
+  description = "The name of the Function App"
+  type        = string
 }
 
 variable "sql_admin_username" {
-  description = "The administrator username for Azure SQL Server"
+  description = "The SQL admin username"
   type        = string
 }
 
 variable "sql_admin_password" {
-  description = "The administrator password for Azure SQL Server"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_ami_id" {
-  description = "The AMI ID for the AWS EC2 instance"
+  description = "The SQL admin password"
   type        = string
 }
 
-variable "db_username" {
-  description = "The username for the AWS RDS database"
+variable "sql_server_name" {
+  description = "The SQL server name"
   type        = string
 }
 
-variable "db_password" {
-  description = "The password for the AWS RDS database"
+variable "sql_database_name" {
+  description = "The SQL database name"
   type        = string
-  sensitive   = true
+}
+
+variable "monitor_name" {
+  description = "The name of the Azure Monitor"
+  type        = string
+}
+
+variable "app_insights_name" {
+  description = "The name of the Application Insights"
+  type        = string
 }
