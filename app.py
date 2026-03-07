cc42aa81343424d5af1484781b71282e8f13cd56
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-03-07 21:38:54 +0530

    Delete app.py

--- app.py
+++
@@ -1,6 +0,0 @@
-# Epic Title: Implement user authentication and authorization features
-
-from backend.authentication.controllers.login_controller import app
-
-if __name__ == "__main__":
-    app.run(debug=True)
