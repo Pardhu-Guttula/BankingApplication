2bb7aaae733cee0dd499175fab8407bcb9a28926
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-03-07 22:25:53 +0530

    commited by n8n

--- database/schema.sql
+++ database/schema.sql
@@ -4,5 +4,6 @@ CREATE TABLE IF NOT EXISTS interactions (
     interaction_id VARCHAR(50) PRIMARY KEY,
     user_id VARCHAR(50),
     interaction_type VARCHAR(50),
-    timestamp TIMESTAMP
+    timestamp TIMESTAMP,
+    location VARCHAR(100)
 );
