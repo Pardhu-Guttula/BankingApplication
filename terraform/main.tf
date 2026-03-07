15be80099476ed0776ce9a05fb81857e83ab67e9
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-03-07 11:48:26 +0530

    arch-gen

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 4.56.0"
     }
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
   }
 }
 
@@ -15,74 +11,90 @@ provider "azurerm" {
   features {}
 }
 
-provider "aws" {
-  region = var.aws_region
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = var.azure_location
-}
-
-resource "azurerm_app_service_plan" "example" {
-  name                = "example-appserviceplan"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  sku {
-    tier = "Basic"
-    size = "B1"
-  }
-}
-
-resource "azurerm_app_service" "example" {
-  name                = "example-appservice"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
-}
-
-resource "azurerm_sql_server" "example" {
-  name                         = "example-sqlserver"
-  resource_group_name          = azurerm_resource_group.example.name
-  location                     = azurerm_resource_group.example.location
-  version                      = "12.0"
-  administrator_login          = var.sql_admin_username
-  administrator_login_password = var.sql_admin_password
-}
-
-resource "azurerm_sql_database" "example" {
-  name                = "example-sqldatabase"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  server_name         = azurerm_sql_server.example.name
-  sku_name            = "S0"
-}
-
-resource "azurerm_storage_account" "example" {
-  name                     = "examplestorageacc"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "aws_instance" "example" {
-  ami           = var.aws_ami_id
-  instance_type = "t2.micro"
+resource "azurerm_key_vault" "vault" {
+  name                     = var.key_vault_name
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  sku_name                 = "standard"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "example-s3-bucket"
-  acl    = "private"
+resource "azurerm_api_management" "api" {
+  name                = var.api_management_name
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  publisher_name      = "MyCompany"
+  publisher_email     = "api@mycompany.com"
+  sku_name            = "Developer"
 }
 
-resource "aws_db_instance" "example" {
-  identifier              = "example-db-instance"
-  allocated_storage       = 10
-  engine                  = "mysql"
-  instance_class          = "db.t2.micro"
-  name                    = "exampledb"
-  master_username         = var.db_username
-  master_password         = var.db_password
-  skip_final_snapshot     = true
+resource "azurerm_function_app" "functions" {
+  name                      = var.function_app_name
+  resource_group_name       = azurerm_resource_group.main.name
+  location                  = azurerm_resource_group.main.location
+  storage_account_name      = azurerm_storage_account.storage.name
+  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
+  site_config {
+    app_settings = {
+      FUNCTIONS_WORKER_RUNTIME = "node"
+    }
+  }
+}
+
+resource "azurerm_sql_server" "sqlserver" {
+  name                         = var.sql_server_name
+  resource_group_name          = azurerm_resource_group.main.name
+  location                    = azurerm_resource_group.main.location
+  version                     = "12.0"
+  administrator_login         = var.sql_admin_username
+  administrator_login_password = var.sql_admin_password
+}
+
+resource "azurerm_sql_database" "sqldb" {
+  name                = var.sql_database_name
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  server_name         = azurerm_sql_server.sqlserver.name
+  sku_name            = "S0"
+}
+
+resource "azurerm_monitor_diagnostic_setting" "monitor" {
+  name                       = var.monitor_name
+  target_resource_id         = azurerm_sql_database.sqldb.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.logs.id
+  enabled_log {
+    category = "SQLSecurityAuditEvents"
+    enabled  = true
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+  metric {
+    category = "AllMetrics"
+    enabled  = true
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+}
+
+resource "azurerm_application_insights" "appinsights" {
+  name                = var.app_insights_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  application_type    = "web"
 }
