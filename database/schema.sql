dfbc91de772948348f748ac64b83afa20fd40181
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-03-07 22:34:44 +0530

    commited by n8n

--- database/schema.sql
+++ database/schema.sql
@@ -2,14 +2,6 @@
 
 -- Layout table
 CREATE TABLE IF NOT EXISTS layout (
-    name VARCHAR(255) PRIMARY KEY
-);
-
--- Component table
-CREATE TABLE IF NOT EXISTS component (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    position INT NOT NULL,
-    layout_name VARCHAR(255),
-    FOREIGN KEY (layout_name) REFERENCES layout(name)
+    name VARCHAR(255) PRIMARY KEY,
+    components TEXT NOT NULL
 );
