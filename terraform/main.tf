b6cf5da6397ffbb1ee2a039f124c2bdbe5213d9d
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-03-07 15:03:08 +0530

    arch-gen

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,100 +1,120 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.56.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "main" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
-resource "azurerm_storage_account" "storage" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.main.name
-  location                 = azurerm_resource_group.main.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_key_vault" "vault" {
-  name                     = var.key_vault_name
-  resource_group_name      = azurerm_resource_group.main.name
-  location                 = azurerm_resource_group.main.location
-  sku_name                 = "standard"
-}
-
-resource "azurerm_api_management" "api" {
-  name                = var.api_management_name
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  publisher_name      = "MyCompany"
-  publisher_email     = "api@mycompany.com"
-  sku_name            = "Developer"
-}
-
-resource "azurerm_function_app" "functions" {
-  name                      = var.function_app_name
-  resource_group_name       = azurerm_resource_group.main.name
-  location                  = azurerm_resource_group.main.location
-  storage_account_name      = azurerm_storage_account.storage.name
-  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
-  site_config {
-    app_settings = {
-      FUNCTIONS_WORKER_RUNTIME = "node"
+resource "azurerm_frontdoor" "afd" {
+  name                = "example-afd"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  enforce_backend_pools_certificate_name_check = false
+  backend_pool_health_probes = {
+    "example-probe" = {
+      probe_method  = "GET"
+      probe_path    = "/index.html"
+      probe_protocol = "Https"
+    }
+  }
+  backend_pools = {
+    "example" = {
+      backends = [{
+        address = "example-backend.azurewebsites.net"
+        host_header = "example-backend.azurewebsites.net"
+      }]
+      load_balancing_name = "example"
+      health_probe_name   = "example-probe"
     }
   }
 }
 
-resource "azurerm_sql_server" "sqlserver" {
-  name                         = var.sql_server_name
-  resource_group_name          = azurerm_resource_group.main.name
-  location                    = azurerm_resource_group.main.location
-  version                     = "12.0"
-  administrator_login         = var.sql_admin_username
-  administrator_login_password = var.sql_admin_password
+resource "azurerm_cdn_profile" "cdn" {
+  name                = "example-cdn"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  sku                 = "Standard_Microsoft"
+}
+
+resource "azurerm_app_service" "portal" {
+  name                = "example-portal"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  app_service_plan_id = azurerm_app_service_plan.plan.id
+}
+
+resource "azurerm_api_management" "apim" {
+  name                = "example-apim"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  publisher_name      = "example-publisher"
+  publisher_email     = "publisher@example.com"
+  sku_name            = "Developer_1"
+}
+
+resource "azurerm_key_vault" "kv" {
+  name                = "example-kv"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+}
+
+resource "azurerm_redis_cache" "redis" {
+  name                = "example-redis"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  capacity            = 1
+  family              = "C"
 }
 
 resource "azurerm_sql_database" "sqldb" {
-  name                = var.sql_database_name
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  server_name         = azurerm_sql_server.sqlserver.name
-  sku_name            = "S0"
+  name                = "example-sqldb"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  server_name         = azurerm_sql_server.server.name
 }
 
-resource "azurerm_monitor_diagnostic_setting" "monitor" {
-  name                       = var.monitor_name
-  target_resource_id         = azurerm_sql_database.sqldb.id
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.logs.id
+resource "azurerm_storage_account" "blob" {
+  name                = "exampleblob"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  account_tier        = var.storage_account_tier
+  account_kind        = "StorageV2"
+}
+
+resource "azurerm_servicebus_namespace" "sbn" {
+  name                = "example-sbn"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  sku                 = "Standard"
+}
+
+resource "azurerm_servicebus_queue" "sq" {
+  name                = "example-sq"
+  resource_group_name = var.resource_group_name
+  namespace_name      = azurerm_servicebus_namespace.sbn.name
+}
+
+resource "azurerm_monitor_diagnostic_setting" "monitor_diagnostics" {
+  name                       = "example-monitor-diagnostics"
+  target_resource_id         = azurerm_app_service.portal.id
+  log_analytics_workspace_id = var.log_analytics_workspace_id
   enabled_log {
-    category = "SQLSecurityAuditEvents"
-    enabled  = true
+    category       = "AppServiceConsoleLogs"
     retention_policy {
-      days    = 0
-      enabled = false
+      enabled = true
+      days    = 7
     }
   }
   metric {
     category = "AllMetrics"
     enabled  = true
-    retention_policy {
-      days    = 0
-      enabled = false
-    }
   }
 }
 
-resource "azurerm_application_insights" "appinsights" {
-  name                = var.app_insights_name
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+resource "azurerm_application_insights" "app_insights" {
+  name                = "example-app-insights"
+  resource_group_name = var.resource_group_name
+  location            = var.location
   application_type    = "web"
+}
+
+resource "azurerm_defender_for_cloud" "defender" {
+  resource_group_name = var.resource_group_name
 }
