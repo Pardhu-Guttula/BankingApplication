851b8c0553119a707aa9a620e96a44a9aee1a6ba
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-03-07 22:34:02 +0530

    commited by n8n

--- app.py
+++ app.py
@@ -2,11 +2,9 @@
 
 from flask import Flask
 from backend.controllers.nav_controller import nav_bp
-from backend.controllers.header_controller import header_bp
 
 app = Flask(__name__)
-app.register_blueprint(nav_bp, url_prefix='/api')
-app.register_blueprint(header_bp, url_prefix='/api')
+app.register_blueprint(nav_bp)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
